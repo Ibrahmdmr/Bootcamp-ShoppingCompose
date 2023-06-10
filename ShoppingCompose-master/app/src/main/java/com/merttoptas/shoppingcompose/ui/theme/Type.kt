7c855e359844ef397636cff628a4452352f55f6d
İbrahim Demir <92909637+Ibrahmdmr@users.noreply.github.com>
2023-06-10 15:11:35 +0300

    Update Type.kt

--- ShoppingCompose-master/app/src/main/java/com/merttoptas/shoppingcompose/ui/theme/Type.kt
+++ ShoppingCompose-master/app/src/main/java/com/merttoptas/shoppingcompose/ui/theme/Type.kt
@@ -1,4 +1,4 @@
-package com.merttoptas.shoppingcompose.ui.theme
+package com.ibrahimdmr.shoppingcompose.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
