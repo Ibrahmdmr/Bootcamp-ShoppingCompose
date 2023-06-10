4ca881de44b5f7ab2adec498f936e27f331be429
İbrahim Demir <92909637+Ibrahmdmr@users.noreply.github.com>
2023-06-10 15:19:35 +0300

    Update ProductRepositoryImpl.kt

--- ShoppingCompose-master/app/src/main/java/com/merttoptas/shoppingcompose/core/data/remote/repository/impl/ProductRepositoryImpl.kt
+++ ShoppingCompose-master/app/src/main/java/com/merttoptas/shoppingcompose/core/data/remote/repository/impl/ProductRepositoryImpl.kt
@@ -1,4 +1,4 @@
-package com.merttoptas.shoppingcompose.core.data.remote.repository.impl
+package com.ibrahimdmr.shoppingcompose.core.data.remote.repository.impl
 
 import com.merttoptas.shoppingcompose.core.data.model.ProductResponse
 import com.merttoptas.shoppingcompose.core.data.remote.repository.ProductRepository
