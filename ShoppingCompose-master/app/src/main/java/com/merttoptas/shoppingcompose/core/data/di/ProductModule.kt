4aa3b516d354d5ae66072af2a8650d1a742423a4
İbrahim Demir <92909637+Ibrahmdmr@users.noreply.github.com>
2023-06-10 15:25:08 +0300

    Update ProductModule.kt

--- ShoppingCompose-master/app/src/main/java/com/merttoptas/shoppingcompose/core/data/di/ProductModule.kt
+++ ShoppingCompose-master/app/src/main/java/com/merttoptas/shoppingcompose/core/data/di/ProductModule.kt
@@ -1,17 +1,13 @@
 package com.ibrahimdmr.shoppingcompose.core.data.di
 
-import com.merttoptas.shoppingcompose.core.data.remote.repository.ProductRepository
-import com.merttoptas.shoppingcompose.core.data.remote.repository.impl.ProductRepositoryImpl
-import com.merttoptas.shoppingcompose.core.data.remote.source.ProductRemoteDataImpl
-import com.merttoptas.shoppingcompose.core.data.remote.source.ProductRemoteDataSource
+import com.ibrahimdmr.shoppingcompose.core.data.remote.repository.ProductRepository
+import com.ibrahimdmr.shoppingcompose.core.data.remote.repository.impl.ProductRepositoryImpl
+import com.ibrahimdmr.shoppingcompose.core.data.remote.source.ProductRemoteDataImpl
+import com.ibrahimdmr.shoppingcompose.core.data.remote.source.ProductRemoteDataSource
 import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
 import dagger.hilt.android.components.ViewModelComponent
-
-/**
- * Created by mertcantoptas on 01.06.2023
- */
 
 @Module
 @InstallIn(ViewModelComponent::class)
