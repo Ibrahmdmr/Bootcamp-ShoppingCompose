e8c2cfcf6fc43f5f85c6200a5a698edc0adb396b
İbrahim Demir <92909637+Ibrahmdmr@users.noreply.github.com>
2023-06-10 15:27:23 +0300

    Update AuthRepositoryImpl.kt

--- ShoppingCompose-master/app/src/main/java/com/merttoptas/shoppingcompose/core/data/remote/repository/impl/AuthRepositoryImpl.kt
+++ ShoppingCompose-master/app/src/main/java/com/merttoptas/shoppingcompose/core/data/remote/repository/impl/AuthRepositoryImpl.kt
@@ -1,13 +1,9 @@
 package com.ibrahimdmr.shoppingcompose.core.data.remote.repository.impl
 
-import com.merttoptas.shoppingcompose.core.data.model.LoginResponse
-import com.merttoptas.shoppingcompose.core.data.remote.repository.AuthRepository
-import com.merttoptas.shoppingcompose.core.data.remote.source.AuthRemoteDataSource
+import com.ibrahimdmr.shoppingcompose.core.data.model.LoginResponse
+import com.ibrahimdmr.shoppingcompose.core.data.remote.repository.AuthRepository
+import com.ibrahimdmr.shoppingcompose.core.data.remote.source.AuthRemoteDataSource
 import javax.inject.Inject
-
-/**
- * Created by mertcantoptas on 26.05.2023
- */
 
 class AuthRepositoryImpl @Inject constructor(
     private val authRemoteDataSource: AuthRemoteDataSource
