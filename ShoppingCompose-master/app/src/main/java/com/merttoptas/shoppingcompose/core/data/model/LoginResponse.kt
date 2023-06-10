57167962efe2ffc742f5d64d3114bbf954eb66f9
İbrahim Demir <92909637+Ibrahmdmr@users.noreply.github.com>
2023-06-10 15:26:14 +0300

    Update LoginResponse.kt

--- ShoppingCompose-master/app/src/main/java/com/merttoptas/shoppingcompose/core/data/model/LoginResponse.kt
+++ ShoppingCompose-master/app/src/main/java/com/merttoptas/shoppingcompose/core/data/model/LoginResponse.kt
@@ -2,10 +2,6 @@ package com.ibrahimdmr.shoppingcompose.core.data.model
 
 import com.google.gson.annotations.SerializedName
 
-/**
- * Created by mertcantoptas on 26.05.2023
- */
-
 data class LoginResponse(
     @SerializedName("token")
     val token: String?
