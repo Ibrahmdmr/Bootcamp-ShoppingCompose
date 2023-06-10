4e09a6703d51a599ed828f2448c7d490baef4399
İbrahim Demir <92909637+Ibrahmdmr@users.noreply.github.com>
2023-06-10 15:29:03 +0300

    Update ProductRemoteDataSource.kt

--- ShoppingCompose-master/app/src/main/java/com/merttoptas/shoppingcompose/core/data/remote/source/ProductRemoteDataSource.kt
+++ ShoppingCompose-master/app/src/main/java/com/merttoptas/shoppingcompose/core/data/remote/source/ProductRemoteDataSource.kt
@@ -2,9 +2,6 @@ package com.ibrahimdmr.shoppingcompose.core.data.remote.source
 
 import com.ibrahimdmr.shoppingcompose.core.data.model.ProductResponse
 
-/**
- * Created by mertcantoptas on 01.06.2023
- */
 interface ProductRemoteDataSource {
     suspend fun getProducts(): Result<List<ProductResponse>>
     suspend fun getProductDetail(id: Int): Result<ProductResponse>
