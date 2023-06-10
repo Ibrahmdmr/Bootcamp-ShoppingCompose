61bd8fd812f0c1a05d82561c0fa9ae747ecd04ef
İbrahim Demir <92909637+Ibrahmdmr@users.noreply.github.com>
2023-06-10 15:15:55 +0300

    Update MainNavHost.kt

--- ShoppingCompose-master/app/src/main/java/com/merttoptas/shoppingcompose/feature/navigation/MainNavHost.kt
+++ ShoppingCompose-master/app/src/main/java/com/merttoptas/shoppingcompose/feature/navigation/MainNavHost.kt
@@ -1,4 +1,4 @@
-package com.merttoptas.shoppingcompose.feature.navigation
+package com.ibrahimdmr.shoppingcompose.feature.navigation
 
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
