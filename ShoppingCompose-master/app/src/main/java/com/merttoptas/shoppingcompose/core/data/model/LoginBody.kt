f3048fb096adf1f8e703131245958ffcf61a8709
İbrahim Demir <92909637+Ibrahmdmr@users.noreply.github.com>
2023-06-10 15:26:01 +0300

    Update LoginBody.kt

--- ShoppingCompose-master/app/src/main/java/com/merttoptas/shoppingcompose/core/data/model/LoginBody.kt
+++ ShoppingCompose-master/app/src/main/java/com/merttoptas/shoppingcompose/core/data/model/LoginBody.kt
@@ -1,9 +1,5 @@
 package com.ibrahimdmr.shoppingcompose.core.data.model
 
-/**
- * Created by mertcantoptas on 26.05.2023
- */
-
 data class LoginBody(
     val email: String,
     val password: String
