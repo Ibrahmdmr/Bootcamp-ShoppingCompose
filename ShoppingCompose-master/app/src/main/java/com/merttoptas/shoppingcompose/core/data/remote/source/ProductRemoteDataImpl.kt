55c04bf81216a08db7b28f4d6c3ad6e17cdb50ab
İbrahim Demir <92909637+Ibrahmdmr@users.noreply.github.com>
2023-06-10 15:28:51 +0300

    Update ProductRemoteDataImpl.kt

--- ShoppingCompose-master/app/src/main/java/com/merttoptas/shoppingcompose/core/data/remote/source/ProductRemoteDataImpl.kt
+++ ShoppingCompose-master/app/src/main/java/com/merttoptas/shoppingcompose/core/data/remote/source/ProductRemoteDataImpl.kt
@@ -4,9 +4,6 @@ import com.ibrahimdmr.shoppingcompose.core.data.model.ProductResponse
 import com.ibrahimdmr.shoppingcompose.core.data.remote.api.ProductService
 import javax.inject.Inject
 
-/**
- * Created by mertcantoptas on 01.06.2023
- */
 class ProductRemoteDataImpl @Inject constructor(
     private val productService: ProductService
 ) : ProductRemoteDataSource {
