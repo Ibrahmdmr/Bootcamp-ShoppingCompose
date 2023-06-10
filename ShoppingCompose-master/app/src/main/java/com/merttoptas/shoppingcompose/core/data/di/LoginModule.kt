1dcf230818c1f0c3fc1ddf6cca92e731dcb5155a
İbrahim Demir <92909637+Ibrahmdmr@users.noreply.github.com>
2023-06-10 15:24:44 +0300

    Update LoginModule.kt

--- ShoppingCompose-master/app/src/main/java/com/merttoptas/shoppingcompose/core/data/di/LoginModule.kt
+++ ShoppingCompose-master/app/src/main/java/com/merttoptas/shoppingcompose/core/data/di/LoginModule.kt
@@ -1,17 +1,13 @@
 package com.ibrahimdmr.shoppingcompose.core.data.di
 
-import com.merttoptas.shoppingcompose.core.data.remote.repository.AuthRepository
-import com.merttoptas.shoppingcompose.core.data.remote.repository.impl.AuthRepositoryImpl
-import com.merttoptas.shoppingcompose.core.data.remote.source.AuthRemoteDataSource
-import com.merttoptas.shoppingcompose.core.data.remote.source.AuthRemoteDataSourceImpl
+import com.ibrahimdmr.shoppingcompose.core.data.remote.repository.AuthRepository
+import com.ibrahimdmr.shoppingcompose.core.data.remote.repository.impl.AuthRepositoryImpl
+import com.ibrahimdmr.shoppingcompose.core.data.remote.source.AuthRemoteDataSource
+import com.ibrahimdmr.shoppingcompose.core.data.remote.source.AuthRemoteDataSourceImpl
 import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
 import dagger.hilt.android.components.ViewModelComponent
-
-/**
- * Created by mertcantoptas on 26.05.2023
- */
 
 @Module
 @InstallIn(ViewModelComponent::class)
