afc43a8605bab0a7ec169ea1e946e8f89f76e1f5
İbrahim Demir <92909637+Ibrahmdmr@users.noreply.github.com>
2023-06-10 15:30:21 +0300

    Update SplashNavigation.kt

--- ShoppingCompose-master/app/src/main/java/com/merttoptas/shoppingcompose/feature/splash/navigation/SplashNavigation.kt
+++ ShoppingCompose-master/app/src/main/java/com/merttoptas/shoppingcompose/feature/splash/navigation/SplashNavigation.kt
@@ -2,11 +2,8 @@ package com.ibrahimdmr.shoppingcompose.feature.splash.navigation
 
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.composable
-import com.merttoptas.shoppingcompose.feature.splash.SplashScreenRoute
+import com.ibrahimdmr.shoppingcompose.feature.splash.SplashScreenRoute
 
-/**
- * Created by mertcantoptas on 30.05.2023
- */
 const val SplashNavigationRoute = "splash_route"
 
 fun NavGraphBuilder.splashScreen(navigateToHome: () -> Unit, navigateToLogin: () -> Unit) {
